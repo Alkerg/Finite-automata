2cd15e810a06d088702e19e44f18f94f11c53505
Alkerg <albert.arg500@gmail.com>
2024-11-21 18:47:38 -0500

    initial commit

--- NFA_to_DFA.py
+++ NFA_to_DFA.py
@@ -1,12 +1,12 @@
 def convertir_afnd_a_afd(estados, alfabeto, transiciones, estado_inicial, estados_finales):
-    # Conjunto inicial del AFD (contiene solo el conjunto del estado inicial del AFN)
+    # Conjunto inicial del AFD 
     estado_inicial_afd = frozenset([estado_inicial])
     estados_por_procesar = [estado_inicial_afd]
     estados_visitados = set()
     transiciones_afd = {}
     estados_finales_afd = set()
 
-    # Procesar cada conjunto de estados
+    # Procesamos cada conjunto de estados
     while estados_por_procesar:
         conjunto_actual = estados_por_procesar.pop(0)
         if conjunto_actual in estados_visitados:
@@ -14,11 +14,11 @@ def convertir_afnd_a_afd(estados, alfabeto, transiciones, estado_inicial, estado
 
         estados_visitados.add(conjunto_actual)
 
-        # Verificar si el conjunto actual contiene algún estado final del AFN
+        # Verificamos si el conjunto actual contiene algún estado final del AFND
         if any(estado in estados_finales for estado in conjunto_actual):
             estados_finales_afd.add(conjunto_actual)
 
-        # Calcular transiciones del conjunto actual
+        # Calculamos las transiciones del conjunto actual
         for simbolo in alfabeto:
             nuevos_estados = set()
             for estado in conjunto_actual:
@@ -29,11 +29,11 @@ def convertir_afnd_a_afd(estados, alfabeto, transiciones, estado_inicial, estado
                 nuevo_conjunto = frozenset(nuevos_estados)
                 transiciones_afd[(conjunto_actual, simbolo)] = nuevo_conjunto
 
-                # Si es un nuevo conjunto, agregarlo a la lista de procesamiento
+                # Si es un nuevo conjunto, lo agregamos a la lista de procesamiento
                 if nuevo_conjunto not in estados_visitados:
                     estados_por_procesar.append(nuevo_conjunto)
 
-    # Devolver el AFD en un diccionario
+    # Devolvemos el AFD en un diccionario
     return {
             "estados": estados_visitados,
             "alfabeto": alfabeto,
@@ -43,26 +43,26 @@ def convertir_afnd_a_afd(estados, alfabeto, transiciones, estado_inicial, estado
         }
 
 
-if __name__ == "__main__":
-    # Definición del AFN
-    estados_afn = ["s0", "s1", "s2","s3"]
-    alfabeto = ["a", "b"]
-    transiciones_afn = {
-        ("s0", "a"): ["s1","s2"],
-        ("s1", "a"): ["s2"],
-        ("s1", "b"): ["s1"],
-        ("s2", "a"): ["s2"],
-        ("s2", "b"): ["s3"],
-        ("s3", "a"): ["s1"],
-        ("s3", "b"): ["s3"]
+# Datos del AFND
+estados_afnd = ["s0", "s1", "s2","s3"]
+alfabeto = ["a", "b"]
+transiciones_afnd = {
+    ("s0", "a"): ["s1","s2"],
+    ("s1", "a"): ["s2"],
+    ("s1", "b"): ["s1"],
+    ("s2", "a"): ["s2"],
+    ("s2", "b"): ["s3"],
+    ("s3", "a"): ["s1"],
+    ("s3", "b"): ["s3"]
 
-    }
-    estado_inicial_afn = "s0"
-    estados_finales_afn = ["s3"]
+}
+estado_inicial_afnd = "s0"
+estados_finales_afnd = ["s3"]
 
-# Convertir el AFN a AFD
-afd = convertir_afnd_a_afd(estados_afn,alfabeto,transiciones_afn,estado_inicial_afn,estados_finales_afn)
-# Mostrar el AFD resultante
+# Convertimos el AFND a AFD
+afd = convertir_afnd_a_afd(estados_afnd,alfabeto,transiciones_afnd,estado_inicial_afnd,estados_finales_afnd)
+
+# Datos del AFD obtenido
 print("Estados del AFD:")
 for estado in afd["estados"]:
     print(set(estado))
